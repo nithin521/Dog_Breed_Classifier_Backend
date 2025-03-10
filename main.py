78be3059e5cdce5f0b8a7cc441c16a9227d79484
Nithin Kumar <85955710+nithin521@users.noreply.github.com>
2025-03-10 21:50:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,4 +61,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment or default to 5000
+    app.run(host="0.0.0.0", port=port)
